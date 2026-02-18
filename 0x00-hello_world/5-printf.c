f097c41c5875c67908a36d1d96394647b1318f9c
derytech <samuelaanyemdery@gmail.com>
2026-02-18 05:58:34 -0800

    Task 5: Print sentence using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a sentence with proper grammar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
