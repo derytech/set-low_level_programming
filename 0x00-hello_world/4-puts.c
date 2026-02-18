1ee34d829a41b6c1cdbfa76dc00d80f23facab0f
derytech <samuelaanyemdery@gmail.com>
2026-02-18 05:46:08 -0800

    Fix Task 4: Add missing opening double quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - print a specific sentence
+ * main - prints a specific sentence
  *
- *Return: Aways 0
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-		return(0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+
