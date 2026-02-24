f58cdc3dd4d2c1949f515aacaa4e2770931b8383
derytech <samuelaanyemdery@gmail.com>
2026-02-24 11:55:21 -0800

    Fix Betty style and add jack_bauer function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * jack_bauer - prints every minute of the day from 00:00 to 23:59
  *
  * Return: void
  */
 void jack_bauer(void)
 {
-    int h, m;
+	int hour, min;
 
-    for (h = 0; h < 24; h++)
-    {
-        for (m = 0; m < 60; m++)
-        {
-            _putchar((h / 10) + '0');
-            _putchar((h % 10) + '0');
-            _putchar(':');
-            _putchar((m / 10) + '0');
-            _putchar((m % 10) + '0');
-            _putchar('\n');
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + min / 10);
+			_putchar('0' + min % 10);
+			_putchar('\n');
+		}
+	}
 }
