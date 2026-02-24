c493385de02db801cbcaa2ef08f8b07df932f147
derytech <samuelaanyemdery@gmail.com>
2026-02-24 12:10:31 -0800

    Add times_table function and main for 9-times_table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void); /* <- add this line */
+void times_table(void);
 
-#endif
+#endif /* MAIN_H */
