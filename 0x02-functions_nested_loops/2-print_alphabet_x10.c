0d9cb75ace8482041d539087b5dae7694e2a91e1
derytech <samuelaanyemdery@gmail.com>
2026-03-09 08:29:14 -0700

    Task 2: print alphabet 10 times using _putchar twice

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet 10 times in lowercase
- *
- * Return: void
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	int i;
+    int i;
+    char ch;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
-
-		_putchar('\n');
-	}
+    for (i = 0; i < 10; i++)
+    {
+        for (ch = 'a'; ch <= 'z'; ch++)
+            _putchar(ch);
+        _putchar('\n');
+    }
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_alphabet(void);
+int _putchar(char c);       /* prototype for _putchar */
+void print_alphabet(void);  /* optional, if you have Task 1 function */
+void print_alphabet_x10(void); /* THIS is the one your compiler needs */
 
-#endif
+#endif /* MAIN_H */
