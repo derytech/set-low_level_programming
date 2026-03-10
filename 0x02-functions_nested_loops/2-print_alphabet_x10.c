f7368c95852b75d0241e9d8fcf7154b0defedd13
derytech <samuelaanyemdery@gmail.com>
2026-03-09 18:04:29 -0700

    Recreate Task 2: print_alphabet_x10 with only 2 _putchar calls

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Only 2 _putchar calls allowed
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char c;
+    int i, j;
+    char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
 
     for (i = 0; i < 10; i++)
     {
-        for (c = 'a'; c <= 'z'; c++)
-            _putchar(c);
-        _putchar('\n');
+        for (j = 0; j < 26; j++)
+            _putchar(alphabet[j]); /* 1st _putchar */
+        _putchar('\n');            /* 2nd _putchar */
     }
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);       /* prototype for _putchar */
-void print_alphabet(void);  /* optional, if you have Task 1 function */
-void print_alphabet_x10(void); /* THIS is the one your compiler needs */
+int _putchar(char c);
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
