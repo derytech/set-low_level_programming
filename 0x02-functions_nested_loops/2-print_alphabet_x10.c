3bcc21df720bfe1193663282933355ff2227c107
derytech <samuelaanyemdery@gmail.com>
2026-03-09 17:48:23 -0700

    Redo Task 2: print alphabet 10 times using _putchar twice

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the lowercase alphabet 10 times
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
  */
 void print_alphabet_x10(void)
 {
     int i;
-    char ch;
+    char c;
 
     for (i = 0; i < 10; i++)
     {
-        for (ch = 'a'; ch <= 'z'; ch++)
-            _putchar(ch);
+        for (c = 'a'; c <= 'z'; c++)
+            _putchar(c);
         _putchar('\n');
     }
 }
