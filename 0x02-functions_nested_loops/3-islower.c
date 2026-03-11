7aa7779d534ce0a5d46b259453d2f5d5508e7633
derytech <samuelaanyemdery@gmail.com>
2026-03-11 05:14:36 -0700

    0-reset_to_98: update value of int to 98

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * _islower - checks for lowercase character
  * @c: character to check
  *
- * Return: 1 if lowercase, 0 otherwise
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
