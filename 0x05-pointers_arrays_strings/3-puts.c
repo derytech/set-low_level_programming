01849b07a45e3da120513cd5add09018f2c63b70
derytech <samuelaanyemdery@gmail.com>
2026-03-11 16:42:01 -0700

    Task 3: implemented _puts function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line
- * @str: pointer to the string
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
