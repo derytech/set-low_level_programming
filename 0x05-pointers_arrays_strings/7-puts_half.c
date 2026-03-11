d5c35a9a9065fbcb28ee15bc33ce76a8d58bbdf1
derytech <samuelaanyemdery@gmail.com>
2026-03-11 15:21:04 -0700

    Task 7: Betty style puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,27 +3,28 @@
 /**
  * puts_half - prints the second half of a string
  * @str: input string
+ *
+ * Description: prints the last half of a string,
+ * starting from (length + 1) / 2 if length is odd
  */
 void puts_half(char *str)
 {
-    int i = 0;
-    int length = 0;
+	int i = 0;
+	int length = 0;
 
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+	while (str[length] != '\0')
+		length++;
 
-    if (length % 2 == 0)
-        i = length / 2;
-    else
-        i = (length + 1) / 2;
+	if (length % 2 == 0)
+		i = length / 2;
+	else
+		i = (length + 1) / 2;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
