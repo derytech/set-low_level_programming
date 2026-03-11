e33921e6db730e8b5bf0c11dc6d09ac200ebb197
derytech <samuelaanyemdery@gmail.com>
2026-03-11 16:47:01 -0700

    Task 2: fixed Betty style warnings for _strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,14 @@
  * _strlen - returns the length of a string
  * @s: pointer to the string
  *
- * Return: the number of characters in the string
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (s[len] != '\0')
-        len++;
+	while (s[len] != '\0')
+		len++;
 
-    return (len);
+	return (len);
 }
