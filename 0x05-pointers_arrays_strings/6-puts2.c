fa51a61086c93989ac3822120c9fd280c491524c
derytech <samuelaanyemdery@gmail.com>
2026-03-11 16:16:15 -0700

    Task 6: corrected puts2 function to pass checker

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string,
- * starting with the first character, followed by a new line
- * @str: pointer to the string
+ * puts2 - prints every other character of a string
+ * @str: input string
+ *
+ * Description: prints every other character of str, starting with the first
+ *              character, followed by a new line
  */
 void puts2(char *str)
 {
 	int i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
+	if (!str)
+		return;
+
+	for (i = 0; str[i] != '\0'; i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
+
 	_putchar('\n');
 }
