fd8c418f810e8c4fa743784676b6dc6b3d8bfdda
derytech <samuelaanyemdery@gmail.com>
2026-03-11 16:52:16 -0700

    Task 4: fixed Betty style warnings for print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
+ * print_rev - prints a string in reverse
  * @s: pointer to the string
  */
 void print_rev(char *s)
 {
-    int len = 0;
-    int i;
+	int len = 0;
+	int i;
 
-    /* find the length of the string */
-    while (s[len] != '\0')
-        len++;
+	while (s[len] != '\0')
+		len++;
 
-    /* print the string in reverse */
-    for (i = len - 1; i >= 0; i--)
-        _putchar(s[i]);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
 
-    /* print a newline at the end */
-    _putchar('\n');
+	_putchar('\n');
 }
