a0c370b2a00ce7fdf8959f80f97801e3c1846678
derytech <samuelaanyemdery@gmail.com>
2026-03-11 17:30:33 -0700

    Task 0: fixed NULL issue and Betty style for reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,13 @@
 #include "main.h"
-#include <stddef.h>  /* for NULL */
+#include <stddef.h>
 
+/**
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: pointer to an integer to be updated
+ *
+ * Description: This function takes a pointer to an int and sets
+ * the value it points to 98.
+ */
 void reset_to_98(int *n)
 {
 	if (n != NULL)
