57912ec5fa2ddb6361be6c7a16bd834562febcd6
derytech <samuelaanyemdery@gmail.com>
2026-03-11 17:24:03 -0700

    Task 0: fixed Betty warnings and NULL usage for reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,8 @@
+#include "main.h"
+#include <stddef.h>  /* for NULL */
+
 void reset_to_98(int *n)
 {
-	if (n != NULL)  // <- make sure the start of this line has a tab, not spaces
+	if (n != NULL)
 		*n = 98;
 }
