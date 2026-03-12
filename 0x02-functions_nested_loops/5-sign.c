cf5394e040e31ec01faccc21f4365a7c499cd2cc
derytech <samuelaanyemdery@gmail.com>
2026-03-11 18:54:37 -0700

    Add _islower, _isalpha, print_sign functions and their main test files, Betty-compliant

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,11 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: number to check
+ * @n: the number to check
  *
- * Return: 1 if n > 0, 0 if n = 0, -1 if n < 0
+ * Return: 1 and prints '+' if n > 0
+ *         0 and prints '0' if n == 0
+ *        -1 and prints '-' if n < 0
  */
 int print_sign(int n)
 {
