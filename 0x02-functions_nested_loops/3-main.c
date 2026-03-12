ceaaa913a40016141e4d4484ccd276444b450c8d
derytech <samuelaanyemdery@gmail.com>
2026-03-11 18:24:18 -0700

    Add _islower function and test main file

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the _islower function.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	int r;
+    int r;
 
-	r = _islower('H');
-	_putchar(r + '0');
+    r = _islower('H');
+    _putchar(r + '0');  /* Should print 0 */
+    r = _islower('o');
+    _putchar(r + '0');  /* Should print 1 */
+    r = _islower(108);
+    _putchar(r + '0');  /* 108 is 'l', should print 1 */
+    _putchar('\n');
 
-	r = _islower('o');
-	_putchar(r + '0');
-
-	r = _islower(108);
-	_putchar(r + '0');
-
-	_putchar('\n');
-
-	return (0);
+    return (0);
 }
