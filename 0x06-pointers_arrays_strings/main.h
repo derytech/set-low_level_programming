0aadbfffc884341b0a8a1a350a8454d422aba023
derytech <samuelaanyemdery@gmail.com>
2026-03-13 02:46:29 -0700

    0x06: Add main.h with all function prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,30 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Standard library inclusion for size_t if needed */
-#include <stddef.h>
-
-/* Function prototypes */
-
-/* 0-strcat.c */
+/* 0x06: String and array functions prototypes */
 char *_strcat(char *dest, char *src);
-
-/* 1-strncat.c */
 char *_strncat(char *dest, char *src, int n);
-
-/* 2-strncpy.c */
 char *_strncpy(char *dest, char *src, int n);
-
-/* 3-strcmp.c */
 int _strcmp(char *s1, char *s2);
-
-/* 4-rev_array.c */
 void reverse_array(int *a, int n);
-
-/* 5-string_toupper.c */
-char *string_toupper(char *);
-
-/* 6-cap_string.c */
-char *cap_string(char *);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
 
 #endif /* MAIN_H */
