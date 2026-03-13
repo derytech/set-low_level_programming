5e67c4043799dce866db79fa65848ae381f83a16
derytech <samuelaanyemdery@gmail.com>
2026-03-13 02:29:56 -0700

    0x06: Implement string and array functions (_strcat, _strncat, _strncpy, _strcmp, reverse_array, string_toupper, cap_string)

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *str);
+/* Standard library inclusion for size_t if needed */
+#include <stddef.h>
 
-#endif
+/* Function prototypes */
+
+/* 0-strcat.c */
+char *_strcat(char *dest, char *src);
+
+/* 1-strncat.c */
+char *_strncat(char *dest, char *src, int n);
+
+/* 2-strncpy.c */
+char *_strncpy(char *dest, char *src, int n);
+
+/* 3-strcmp.c */
+int _strcmp(char *s1, char *s2);
+
+/* 4-rev_array.c */
+void reverse_array(int *a, int n);
+
+/* 5-string_toupper.c */
+char *string_toupper(char *);
+
+/* 6-cap_string.c */
+char *cap_string(char *);
+
+#endif /* MAIN_H */
