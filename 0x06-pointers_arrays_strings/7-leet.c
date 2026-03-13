2cc4b6f4efdcb640a4838f28d99e81d957f6c034
derytech <samuelaanyemdery@gmail.com>
2026-03-13 02:50:43 -0700

    7-leet: encode string to 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: pointer to encoded string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char nums[] = "4433007711";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; letters[j]; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = nums[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
