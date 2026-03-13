c9aa8ad8d5b424e11225aca511d4f6833d01dc15
derytech <samuelaanyemdery@gmail.com>
2026-03-13 05:25:37 -0700

    0-memset: implement _memset function

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       in 0x07-pointers_arrays_strings project
+ */
+
+/* 0-memset.c */
+char *_memset(char *s, char b, unsigned int n);
+
+/* Future function prototypes can go here */
+/* e.g., 1-memcpy.c, 2-strchr.c, etc. */
+
+#endif /* MAIN_H */
