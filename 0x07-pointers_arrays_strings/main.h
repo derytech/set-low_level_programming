8cabe68cb105b4005710edf67ce05dfca20802b3
derytech <samuelaanyemdery@gmail.com>
2026-03-13 06:39:56 -0700

    Implement _strstr function

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,10 +3,22 @@
 
 #include <stddef.h>
 
+/* 0-memset.c */
 char *_memset(char *s, char b, unsigned int n);
+
+/* 1-memcpy.c */
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* 2-strchr.c */
 char *_strchr(char *s, char c);
+
+/* 3-strspn.c */
 unsigned int _strspn(char *s, char *accept);
+
+/* 4-strpbrk.c */
 char *_strpbrk(char *s, char *accept);
 
-#endif
+/* 5-strstr.c */
+char *_strstr(char *haystack, char *needle);
+
+#endif /* MAIN_H */
