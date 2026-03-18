bffd1361cd50d7cf2b339a361ac41cbda4072cbd
derytech <samuelaanyemdery@gmail.com>
2026-03-17 18:48:17 -0700

    Fix Betty style for _puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a newline
+ * _puts_recursion - prints a string followed by a new line
  * @s: pointer to the string
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')  /* base case: end of string */
-    {
-        _putchar('\n');
-        return;
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-    _putchar(*s);          /* print current character */
-    _puts_recursion(s + 1); /* recursive call */
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
