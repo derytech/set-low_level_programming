532503be2de50cc3ce0d27a1582eddb397c23616
derytech <samuelaanyemdery@gmail.com>
2026-05-07 01:48:33 -0700

    Fix Betty style for create_array function

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,27 +4,27 @@
 /**
  * create_array - creates an array of chars and initializes it
  * @size: size of the array
- * @c: character to initialize array with
+ * @c: character to fill array
  *
- * Return: pointer to array, or NULL if it fails
+ * Return: pointer to array or NULL
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int i;
+	char *array;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    array = malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 
-    if (array == NULL)
-        return (NULL);
+	if (array == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-    {
-        array[i] = c;
-    }
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
 
-    return (array);
+	return (array);
 }
