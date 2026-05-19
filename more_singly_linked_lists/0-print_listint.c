03cd2904c39f86229eec647186a19d00d487f72f
derytech <samuelaanyemdery@gmail.com>
2026-05-19 04:52:28 -0700

    Fix print_listint function and Betty compliance

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -3,20 +3,20 @@
 
 /**
  * print_listint - prints all elements of a listint_t list
- * @h: pointer to the first node
+ * @h: pointer to head node
  *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
